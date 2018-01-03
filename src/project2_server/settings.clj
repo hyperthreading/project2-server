ba8745c5ae2ee6ee57861237985a966a090d7573
hyperthreading <no1expman@gmail.com>
2018-01-03 21:22:48 +0900

    Bugfix on settings.clj

--- src/project2_server/settings.clj
+++ src/project2_server/settings.clj
@@ -1,14 +1,13 @@
 (ns project2-server.settings)
 
-(def require-auth (boolean (or (System/getenv "WITHOUT_AUTH")
-                               true)))
+(def require-auth (boolean (not (System/getenv "WITHOUT_AUTH"))))
 
 (def server-location (or (System/getenv "SERVER_LOCATION")
                          "http://localhost:3000"))
 (def mongo-host (or (System/getenv "MONGO_HOST")
                     "localhost"))
 (def mongo-port (Integer. (or (System/getenv "MONGO_PORT")
-                              27107)))
+                              27017)))
 (def mongo-dbname "syncsync")
 (def mongo-images "images")
 (def mongo-music "music")
